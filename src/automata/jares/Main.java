0a907e7b5c8d42f5997cf97d73669d99d9babe1c
Caleb Jares <cable729@gmail.com>
2013-11-18 21:39:05 -0600

    added file input redirection :tractor:

--- src/automata/jares/Main.java
+++ src/automata/jares/Main.java
@@ -1,8 +1,21 @@
 package automata.jares;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanny = new Scanner(System.in);
+
+        String input = scanny.nextLine();
+        while (scanny.hasNextLine()) {
+            String line = scanny.nextLine();
+            if (true) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
     }
 }
